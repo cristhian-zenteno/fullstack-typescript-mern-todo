a3545ed52ac5bfebac5134210a28f40f7b7e1640
Cristhian Zenteno <cristhian.zenteno@ascbominimac61.local>
2021-05-02 09:52:51 -0400

    fix project

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,20 +7,21 @@ const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 4000
 
+app.use(express.json())
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.set('useFindAndModify', false)
+const uri: string = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@localhost:27017`;
 
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.set("useFindAndModify", false);
 mongoose
-    .connect(uri, options)
-    .then(() =>
-        app.listen(PORT, () =>
-            console.log(`Server running on http://localhost:${PORT}`)
-        )
-    )
-    .catch((error) => {
-        throw error
-    })
+  .connect(uri, options)
+  .then(db => {
+    console.log("database connected", db.connection.host)
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => console.log(error));
